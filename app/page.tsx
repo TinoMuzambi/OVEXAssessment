62ede4256528cb12416fbc747cc387426b94cddf
TinoMuzambi <tinomuzambi@gmail.com>
2025-02-19 20:01:04 +0200

    Adds w-full to give card more space.

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,12 @@ import { Suspense } from "react";
 const Home = async () => {
 	const [markets, currencies] = await Promise.all([
 		getMarkets(),
-		getCurrencies()
-	])
+		getCurrencies(),
+	]);
 
 	return (
 		<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-			<main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+			<main className="flex flex-col gap-8 row-start-2 items-center sm:items-start w-full">
 				<Suspense fallback={<div>Loading...</div>}>
 					<RFQ markets={markets} currencies={currencies} />
 				</Suspense>
@@ -18,6 +18,6 @@ const Home = async () => {
 			<footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
 		</div>
 	);
-}
+};
 
 export default Home;
