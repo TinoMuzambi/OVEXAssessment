3d88c01467e28ebba7e6e8bc2bfe20f9d8182e06
Tinotenda Muzambi <tinomuzambi@gmail.com>
2025-02-19 10:25:29 +0000

    Adds quote content.

--- components/Quote.tsx
+++ components/Quote.tsx
@@ -1,7 +1,32 @@
+import { Button } from "./ui/button";
+
 const Quote: React.FC = () => {
 
     return (
-        <div className="mt-6 space-y-4 p-4 bg-muted rounded-lg"></div>
+        <div className="mt-6 space-y-4 p-4 bg-muted rounded-lg">
+            <div className="flex justify-between">
+                <span className="text-muted-foreground">Cost:</span>
+                <span className="font-medium">
+                    ZAR: R800.00
+                </span>
+            </div>
+            <div className="flex justify-between">
+                <span className="text-muted-foreground">Rate:</span>
+                <span className="font-medium">ZAR R8.00</span>
+            </div>
+            <div className="flex justify-between">
+                <span className="text-muted-foreground">You will receive:</span>
+                <span className="font-medium"> BTC</span>
+            </div>
+            <div className="flex justify-between">
+                <span className="text-muted-foreground">Quote expires in:</span>
+                <span className="font-medium">30s</span>
+            </div>
+
+            <Button className="w-full" variant="secondary">
+                Accept Quote
+            </Button>
+        </div>
     )
 
 }
