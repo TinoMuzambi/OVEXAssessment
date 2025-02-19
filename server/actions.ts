f7e270c30828c924d395c8a72f8628c79fc612d6
Tinotenda Muzambi <tinomuzambi@gmail.com>
2025-02-19 13:39:47 +0000

    Implements getCurrencies server action.

--- server/actions.ts
+++ server/actions.ts
@@ -1,6 +1,6 @@
 "use server"
 
-import { BASE_URL, MarketType, QuoteType } from "@/lib/utils";
+import { BASE_URL, CurrencyType, MarketType, QuoteType } from "@/lib/utils";
 
 export async function getMarkets() {
     const res = await fetch(`${BASE_URL}/markets`,
@@ -16,9 +16,18 @@ export async function getMarkets() {
 }
 
 export async function getCurrencies() {
-    
+    const res = await fetch(`${BASE_URL}/currencies`,
+        {
+            next: {
+                revalidate: 86400 // Revalidate at most every day
+            }
+        }
+    )
+    const json: CurrencyType[] = await res.json()
+
+    return json
 }
 
 export async function requestQuote({market, from_amount, side, to_amount}: Pick<QuoteType, 'market' | 'from_amount' | 'side' | 'to_amount'>) {
-    
+    return null
 }
