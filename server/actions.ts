e796b3d69338899deefb34dfde623347f35e4e55
Tinotenda Muzambi <tinomuzambi@gmail.com>
2025-02-19 13:49:45 +0000

    Adds cache force-cache.

--- server/actions.ts
+++ server/actions.ts
@@ -5,6 +5,7 @@ import { BASE_URL, CurrencyType, MarketType, QuoteType } from "@/lib/utils";
 export async function getMarkets() {
     const res = await fetch(`${BASE_URL}/markets`,
         {
+            cache: "force-cache",
             next: {
                 revalidate: 86400 // Revalidate at most every day
             }
@@ -18,6 +19,7 @@ export async function getMarkets() {
 export async function getCurrencies() {
     const res = await fetch(`${BASE_URL}/currencies`,
         {
+            cache: "force-cache",
             next: {
                 revalidate: 86400 // Revalidate at most every day
             }
@@ -29,5 +31,10 @@ export async function getCurrencies() {
 }
 
 export async function requestQuote({market, from_amount, side, to_amount}: Pick<QuoteType, 'market' | 'from_amount' | 'side' | 'to_amount'>) {
+    const res = await fetch(`${BASE_URL}/rfq/get_quote?market=${market}&from_amount=${from_amount}&side=${side}&to_amount=${to_amount}`,
+        {
+            method: "POST"
+        }
+    )
     return null
 }
