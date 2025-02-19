1515326b11fc4b3ad323b5dc9e8a40845536262c
TinoMuzambi <tinomuzambi@gmail.com>
2025-02-19 20:48:19 +0200

    Adds param for precision.

--- components/Quote.tsx
+++ components/Quote.tsx
@@ -24,12 +24,18 @@ const Quote: React.FC<QuoteProps> = ({ quote }) => {
 	 * This function formats a currency amount to a string with the given currency symbol
 	 * @param currency The currency to format
 	 * @param amount The amount to format
+	 * @param precision The number of decimal places to display
 	 * @returns A formatted currency string
 	 */
-	const currencyFormatter = (currency: string, amount: number) => {
+	const currencyFormatter = (
+		currency: string,
+		amount: number,
+		precision = 2
+	) => {
 		return new Intl.NumberFormat("en-ZA", {
 			style: "currency",
 			currency: currency,
+			minimumFractionDigits: precision,
 		}).format(amount);
 	};
 
@@ -54,7 +60,7 @@ const Quote: React.FC<QuoteProps> = ({ quote }) => {
 			<div className="flex justify-between">
 				<span className="text-muted-foreground">You will receive:</span>
 				<span className="font-medium">
-					{currencyFormatter(quote.to_currency, parseFloat(quote.to_amount))}
+					{currencyFormatter(quote.to_currency, parseFloat(quote.to_amount), 6)}
 				</span>
 			</div>
 			<div className="flex justify-between">
