cdc7e18a18dae2933d5d87974bd0405f6f7445b7
TinoMuzambi <tinomuzambi@gmail.com>
2025-02-20 08:13:15 +0200

    Prevents scrolling on home page.

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ const Home = async () => {
 	]);
 
 	return (
-		<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-4 pb-8 gap-8 sm:p-8">
+		<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-4 pb-4 gap-8 sm:p-4">
 			<main className="flex flex-col gap-8 row-start-2 items-center sm:items-start w-full">
 				<Suspense fallback={<Loader2 className="h-8 w-8 animate-spin" />}>
 					<RFQ marketsProp={markets} currenciesProp={currencies} />
