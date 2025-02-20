8630ae0b91d5de4daccc94d1019a5aba218dcc8d
TinoMuzambi <tinomuzambi@gmail.com>
2025-02-20 15:42:35 +0200

    Adds useCallback and useMemo to context for better performance.

--- app/context/AppContext.tsx
+++ app/context/AppContext.tsx
@@ -1,6 +1,12 @@
 "use client";
 
-import { createContext, ReactNode, useReducer } from "react";
+import {
+	createContext,
+	ReactNode,
+	useReducer,
+	useCallback,
+	useMemo,
+} from "react";
 
 import AppReducer from "./AppReducer";
 import {
@@ -22,45 +28,56 @@ export const AppContext = createContext<ContextProps>(initialState);
 export const AppProvider = ({ children }: AppProviderProps): ReactNode => {
 	const [state, dispatch] = useReducer(AppReducer, initialState);
 
-	const setQuote = (quote: QuoteType | undefined) => {
-		dispatch({
-			type: "SET_QUOTE",
-			quote,
-			currencies: state.currencies,
-			markets: state.markets,
-		});
-	};
-
-	const setCurrencies = (currencies: CurrencyType[]) => {
-		dispatch({
-			type: "SET_CURRENCIES",
-			currencies,
-			quote: state.quote,
-			markets: state.markets,
-		});
-	};
-
-	const setMarkets = (markets: MarketType[]) => {
-		dispatch({
-			type: "SET_MARKETS",
-			markets,
-			quote: state.quote,
-			currencies: state.currencies,
-		});
-	};
-
-	return (
-		<AppContext.Provider
-			value={{
-				quote: state.quote,
+	const setQuote = useCallback(
+		(quote: QuoteType | undefined) => {
+			dispatch({
+				type: "SET_QUOTE",
+				quote,
 				currencies: state.currencies,
 				markets: state.markets,
-				setQuote,
-				setCurrencies,
-				setMarkets,
-			}}
-		>
-			{children}
-		</AppContext.Provider>
+			});
+		},
+		[state.currencies, state.markets]
+	);
+
+	const setCurrencies = useCallback(
+		(currencies: CurrencyType[]) => {
+			dispatch({
+				type: "SET_CURRENCIES",
+				currencies,
+				quote: state.quote,
+				markets: state.markets,
+			});
+		},
+		[state.quote, state.markets]
+	);
+
+	const setMarkets = useCallback(
+		(markets: MarketType[]) => {
+			dispatch({
+				type: "SET_MARKETS",
+				markets,
+				quote: state.quote,
+				currencies: state.currencies,
+			});
+		},
+		[state.quote, state.currencies]
+	);
+
+	const contextValue = useMemo(
+		() => ({
+			quote: state.quote,
+			currencies: state.currencies,
+			markets: state.markets,
+			setQuote,
+			setCurrencies,
+			setMarkets,
+		}),
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+		[state.quote, state.currencies, state.markets]
+	);
+
+	return (
+		<AppContext.Provider value={contextValue}>{children}</AppContext.Provider>
 	);
 };
